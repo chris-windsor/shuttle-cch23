cdd84331f8901e6805718e6ecb3398b9a3b71d46
Chris Windsor <ccw3102@icloud.com>
2023-12-01 16:40:02 -0700

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
+use std::path::PathBuf;
+
+use actix_web::{
+    get,
+    web::{self, ServiceConfig},
+    HttpResponse, Responder,
+};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/-1/error")]
@@ -6,10 +12,33 @@ async fn fake_error() -> impl Responder {
     HttpResponse::InternalServerError()
 }
 
+#[get("/1/{tail:.*}")]
+async fn day_1(path: web::Path<PathBuf>) -> impl Responder {
+    let packet_ids = path.into_inner();
+    let packet_ids = packet_ids.as_path();
+
+    let xor_res = packet_ids
+        .iter()
+        .map(|packet_id| {
+            packet_id
+                .to_str()
+                .unwrap()
+                .parse::<i32>()
+                .expect("to parse path")
+        })
+        .reduce(|acc, cur| acc ^ cur)
+        .unwrap();
+
+    let pow_res = xor_res.pow(3);
+
+    HttpResponse::Ok().body(pow_res.to_string())
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(fake_error);
+        cfg.service(day_1);
     };
 
     Ok(config.into())
