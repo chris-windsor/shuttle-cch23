fd21f238cccd27d638700e390ca5233b60a5f05c
Chris Windsor <ccw3102@icloud.com>
2023-12-07 11:28:45 -0700

    day 7

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, path::PathBuf};
 
 use actix_web::{
-    get, post,
+    get,
+    http::header::HeaderValue,
+    post,
     web::{self, ServiceConfig},
-    HttpResponse, Responder, Result,
+    HttpRequest, HttpResponse, Responder, Result,
 };
+use base64::{engine::general_purpose, Engine};
 use serde::Deserialize;
 use serde_json::json;
 use shuttle_actix_web::ShuttleActixWeb;
@@ -147,6 +150,77 @@ async fn day_6(body: web::Bytes) -> Result<impl Responder> {
     })))
 }
 
+fn decode_cookie_header(cookie_header: &HeaderValue) -> String {
+    let header_value = cookie_header.to_str().unwrap();
+    let recipe_encoded = &header_value["recipe=".len()..];
+
+    String::from_utf8(general_purpose::STANDARD.decode(recipe_encoded).unwrap()).unwrap()
+}
+
+#[get("/7/decode")]
+async fn day_7_decode(req: HttpRequest) -> impl Responder {
+    let cookie_header = req.headers().get("Cookie").unwrap();
+    let recipe = decode_cookie_header(&cookie_header);
+
+    HttpResponse::Ok().body(recipe)
+}
+
+#[derive(Debug, Deserialize)]
+struct BakeOrder {
+    recipe: serde_json::Value,
+    pantry: serde_json::Value,
+}
+
+#[get("/7/bake")]
+async fn day_7_bake(req: HttpRequest) -> Result<impl Responder> {
+    let cookie_header = req.headers().get("Cookie").unwrap();
+    let recipe = decode_cookie_header(&cookie_header);
+
+    let bake_order: BakeOrder = serde_json::from_str(recipe.as_str()).unwrap();
+    let recipe = bake_order.recipe.as_object().unwrap();
+    let pantry = bake_order.pantry.as_object().unwrap();
+
+    let mut calc: Vec<_> = recipe
+        .iter()
+        .map(|(item, qty)| {
+            (
+                item,
+                &pantry
+                    .get(item)
+                    .unwrap_or(&serde_json::Value::from(0))
+                    .as_u64()
+                    .unwrap()
+                    / qty.as_u64().unwrap(),
+            )
+        })
+        .collect();
+
+    calc.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+    let max_cookies = calc.get(0).unwrap().1;
+
+    let pantry_balances: HashMap<_, _> = pantry
+        .into_iter()
+        .map(|(item, qty)| {
+            (
+                item,
+                qty.as_u64().unwrap_or_default()
+                    - (max_cookies
+                        * &recipe
+                            .get(item)
+                            .unwrap_or(&serde_json::Value::from(0))
+                            .as_u64()
+                            .unwrap()),
+            )
+        })
+        .collect();
+
+    Ok(web::Json(json!({
+        "cookies": max_cookies,
+        "pantry": pantry_balances
+    })))
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
@@ -155,6 +229,8 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
         cfg.service(day_4_strength);
         cfg.service(day_4_contest);
         cfg.service(day_6);
+        cfg.service(day_7_decode);
+        cfg.service(day_7_bake);
     };
 
     Ok(config.into())
