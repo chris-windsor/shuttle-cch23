eea9ea40d084829c270c6a08f1a8a9237e0adee2
Chris Windsor <ccw3102@icloud.com>
2023-12-12 14:13:37 -0700

    day 12

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,14 @@ use actix_web::{
     HttpRequest, HttpResponse, Responder, Result,
 };
 use base64::{engine::general_purpose, Engine};
+use chrono::{DateTime, Datelike, NaiveTime, Utc, Weekday};
 use image::GenericImageView;
 use serde::Deserialize;
 use serde_json::json;
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_persist::PersistInstance;
+use ulid::Ulid;
+use uuid::Uuid;
 
 #[get("/")]
 async fn base() -> impl Responder {
@@ -291,8 +295,85 @@ async fn day_11_red_pixels(MultipartForm(form): MultipartForm<ImageForm>) -> imp
     magical_red.to_string()
 }
 
+#[post("/12/save/{packet}")]
+async fn day_12_save(packet: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+    let packet = packet.into_inner();
+
+    let _ = data.persist.save::<NaiveTime>(&packet, Utc::now().time());
+
+    HttpResponse::Ok()
+}
+
+#[get("/12/load/{packet}")]
+async fn day_12_load(packet: web::Path<String>, data: web::Data<AppState>) -> impl Responder {
+    let packet = packet.into_inner();
+
+    let current_time = Utc::now().time();
+    let packet_time = data.persist.load::<NaiveTime>(&packet).unwrap();
+
+    let diff = current_time - packet_time.clone();
+
+    diff.num_seconds().to_string()
+}
+
+#[post("/12/ulids")]
+async fn day_12_ulids(ulids: web::Json<Vec<Ulid>>) -> Result<impl Responder> {
+    let uuids = ulids
+        .0
+        .iter()
+        .map(|ulid| Uuid::from(ulid.clone()))
+        .rev()
+        .collect::<Vec<Uuid>>();
+
+    Ok(web::Json(json!(uuids)))
+}
+
+#[post("/12/ulids/{weekday}")]
+async fn day_12_lsb(weekday: web::Path<u8>, ulids: web::Json<Vec<Ulid>>) -> Result<impl Responder> {
+    let weekday = weekday.into_inner();
+    let weekday = Weekday::try_from(weekday).unwrap();
+
+    let (christmas_eve_count, weekday_count, future_count, lsb_count) = ulids.0.iter().fold(
+        (0, 0, 0, 0),
+        |(christmas_eve_cur, weekday_cur, future_cur, lsb_cur), ulid| {
+            let current_time = Utc::now();
+            let ulid_time: DateTime<Utc> = ulid.datetime().into();
+
+            let month_of_year = ulid_time.month();
+            let day_of_month = ulid_time.day();
+            let matches_christmas_eve = month_of_year == 12 && day_of_month == 24;
+
+            let matches_weekday = ulid_time.weekday() == weekday;
+
+            let matches_future = (ulid_time - current_time).num_seconds() > 0;
+
+            let matches_lsb = ulid.random() % (1 << 1) == 1;
+
+            (
+                christmas_eve_cur + (matches_christmas_eve as i32),
+                weekday_cur + (matches_weekday as i32),
+                future_cur + (matches_future as i32),
+                lsb_cur + (matches_lsb as i32),
+            )
+        },
+    );
+
+    Ok(web::Json(json!({
+        "christmas eve": christmas_eve_count,
+        "weekday": weekday_count,
+        "in the future": future_count,
+        "LSB is 1": lsb_count
+    })))
+}
+
+struct AppState {
+    persist: PersistInstance,
+}
+
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn main(
+    #[shuttle_persist::Persist] persist: PersistInstance,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(base);
         cfg.service(fake_error);
@@ -306,9 +387,16 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
         cfg.service(day_8_drop);
         cfg.service(day_11_image);
         cfg.service(day_11_red_pixels);
+        cfg.service(day_12_save);
+        cfg.service(day_12_load);
+        cfg.service(day_12_ulids);
+        cfg.service(day_12_lsb);
 
         // 32MB
         cfg.app_data(web::PayloadConfig::new(1 << 25));
+
+        let app_data = web::Data::new(AppState { persist });
+        cfg.app_data(app_data.clone());
     };
 
     Ok(config.into())
